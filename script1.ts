18d178e0e2c9bccb2b29ca09dafa2935ce3616cb
Татьяна Хонякина <hanakinat@gmail.com>
2023-10-05 16:33:46 +0300

    Исправлены замечания

--- script1.ts
+++ script1.ts
@@ -1,21 +1,19 @@
 // 1.Написать тип для функции, которая принимает один аргумент - число или строку. Если пришло число - возвращает {d: 1}, если пришла строка {z: 2}
 
 // Comment. Нет смысла в этом интерфейсе, в ts есть тип object
-interface Obj {
-    [key: string]: number,
-}
 
 // Comment. Obj - это абсолютно любой объект
-// FIXME. Задача решается через условную типизацию
-type Func = (value: number | string) => Obj;
+// FIXME. Задача решается через условную типизацию (DONE)
 
-const stringOrNumber: Func = (value) => {
-    return (typeof value === "number") ? {d: 1} : {z: 2};
+type NumberOrString<T> = T extends number ? { d: number } : T extends string ? { z: number } : void;
+
+function stringOrNumber<T>(value: T): NumberOrString<T> {
+    return ((typeof value === 'number') ? { d: 1 } : { z: 2 }) as NumberOrString<T>;
 }
 
-// FIXME. Типизация реализована не верно. TS не понимает, что при передаче 2 будет доступно свойство d, а при 'string' z
-const { d }: { d: number } = stringOrNumber(2);
-const { z }: { z: number } = stringOrNumber('string');
+// FIXME. Типизация реализована не верно. TS не понимает, что при передаче 2 будет доступно свойство d, а при 'string' z (DONE)
+const { d }  = stringOrNumber(2);
+const { z } = stringOrNumber('string');
 
 // 2.Реализовать класс SomeClass (DONE)
 
